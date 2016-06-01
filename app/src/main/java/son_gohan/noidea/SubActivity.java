69303e686640b778399c1dc2c3138cd7be1730f2
DhjAsnani <asnani.dheeraj@yahoo.com>
2016-06-01 21:35:21 +0530

    Back Button Implemented

--- app/src/main/java/son_gohan/noidea/SubActivity.java
+++ app/src/main/java/son_gohan/noidea/SubActivity.java
@@ -1,11 +1,15 @@
 package son_gohan.noidea;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class SubActivity extends AppCompatActivity {
 
@@ -13,9 +17,10 @@ public class SubActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
-
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,4 +31,21 @@ public class SubActivity extends AppCompatActivity {
         });
     }
 
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if(id == android.R.id.home)
+        {
+            //TODO : we are going to use NavUtil Class here
+            startActivity(new Intent(this,MainActivity.class));
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
