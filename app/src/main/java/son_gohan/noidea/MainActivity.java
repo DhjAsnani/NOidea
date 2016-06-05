335f4c32108caa677f623d906ad0f9620d997959
dhjasnani <asnani.dheeraj@yahoo.com>
2016-06-05 12:10:27 +0530

    Drawer Fragment

--- app/src/main/java/son_gohan/noidea/MainActivity.java
+++ app/src/main/java/son_gohan/noidea/MainActivity.java
@@ -4,15 +4,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+     private NavigationDrawerFragment drawerFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        drawerFragment = (NavigationDrawerFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        drawerFragment.setup((DrawerLayout)findViewById(R.id.drawer_layout),toolbar);
     }
 
     @Override
